bd5bf5e0d8ef20e41c7343ab4f251598e71ecf86
kyo <a@kyo5884.tk>
2020-03-18 15:29:35 +0900

    add tailwindcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,4 +3,5 @@ module.exports = {
     title: `kyo5884.com`,
     siteUrl: 'https://kyo5884.com',
   },
+  plugins: ['gatsby-plugin-postcss'],
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,2 +1,3 @@
 import React from 'react'
+import '../style/tailwind.css'
 export default () => <h1>404</h1>
