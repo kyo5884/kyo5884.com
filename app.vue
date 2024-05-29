9627fa08c3a4d58611fc216b6b4ffad78628b351
kyo5884 <kyo5884.public@gmail.com>
2024-05-29 18:43:10 +0900

    add meta

--- app.vue
+++ app.vue
@@ -4,6 +4,15 @@
   </main>
 </template>
 
+<script setup lang="ts">
+useSeoMeta({
+  title: 'kyo5884.com',
+  ogTitle: 'kyo5884.com',
+  description: 'Trackmaker, Lyricist, Designer',
+  ogDescription: 'Trackmaker, Lyricist, Designer',
+})
+</script>
+
 <style>
 body {
   font-family: 'Josefin Slab';
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  pages: true,
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    },
+  },
   modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', '@vesp/nuxt-fontawesome'],
   eslint: {
     config: {
