bf155e4b9fea4c83c6bdea4c34e3f4e2ebdf0b61
kyo5884 <kyo5884.public@gmail.com>
2024-07-17 01:29:15 +0900

    add discography

--- pages/index.vue
+++ pages/index.vue
@@ -129,5 +129,97 @@
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
       />
     </section>
+    <section class="p-4 grid grid-cols-3 md:grid-cols-6 gap-4">
+      <AlbumCard
+        name="DTMシンガーソングライターコンピ"
+        url="https://lro.sakura.ne.jp/DTM-SSW/"
+        cover-url="https://lro.sakura.ne.jp/DTM-SSW/images/DTM-SSW_jk.jpg"
+      />
+      <AlbumCard
+        name="だってあたしたち可愛いし"
+        url="https://saikou.world/releases/datte-kawaii/"
+        cover-url="https://images.ctfassets.net/7qcqmfuv8a2o/tlXKX7oBf3u8oLD17myI0/4627d86a4ff1ad30d6d5f37d3f3ff34b/cover-v03.png"
+      />
+      <AlbumCard
+        name="地平線の彼方に君がいない"
+        url="https://saikou.world/releases/chiheisen-no-kanata/"
+        cover-url="https://images.ctfassets.net/7qcqmfuv8a2o/1RAOSnbZ08N0O1niMMYoQx/e6eb0f6c177b2499cf864f9ef95ce62e/cover1600.png"
+      />
+      <AlbumCard
+        name="地獄の果まで連れてって"
+        url="https://saikou.world/releases/jigoku-no-hate/"
+        cover-url="https://images.ctfassets.net/7qcqmfuv8a2o/e31oFqQgQlnH8AZxA0jcA/34f722f3953fa413df1cf0eae99ddb05/jigokuno-hate-480.jpg"
+      />
+      <AlbumCard
+        name="病的に可愛いって言って"
+        url="https://saikou.world/releases/byouteki"
+        cover-url="https://images.ctfassets.net/7qcqmfuv8a2o/4xCcvBxv3xyP9JTw8fo7YM/b5ecebf99c7a6b9b9241a92ecbcd36a3/byouteki-jacket.jpg"
+      />
+      <AlbumCard
+        name="crocco"
+        url="http://www.getsumenhikou.com/crocco/"
+        cover-url="https://www.getsumenhikou.com/content/images/crocco.jpg"
+      />
+      <AlbumCard
+        name="Epic RPG IV"
+        url="http://epicrpg4.bmemo.pw/"
+        cover-url="https://www.getsumenhikou.com/content/images/jacket/epicrpg4.png"
+      />
+      <AlbumCard
+        name="Balboa"
+        url="http://www.getsumenhikou.com/balboa/"
+        cover-url="https://www.getsumenhikou.com/content/images/jacket/jacket.png"
+      />
+      <AlbumCard
+        name="Epic RPG III"
+        url="http://epicrpg3.bmemo.pw/"
+        cover-url="https://www.getsumenhikou.com/content/images/jacket/epicrpg3.jpg"
+      />
+      <AlbumCard
+        name="something mini"
+        url="http://www.getsumenhikou.com/something-mini/"
+        cover-url="https://www.getsumenhikou.com/content/images/jacket/something_mini_jacket.png"
+      />
+      <AlbumCard
+        name="A-Remix Nation 10"
+        url="http://youhardcore.web.fc2.com/release/022/index.html"
+        cover-url="https://www.getsumenhikou.com/content/images/jacket/ARN10JK.jpg"
+      />
+      <AlbumCard
+        name="A-Remix Nation 9"
+        url="http://youhardcore.web.fc2.com/release/020/index.html"
+        cover-url="https://www.getsumenhikou.com/content/images/jacket/ARN9JK500.jpg"
+      />
+      <AlbumCard
+        name="Archytas"
+        url="http://www.getsumenhikou.com/archytas/"
+        cover-url="https://www.getsumenhikou.com/content/images/jacket/archytas_jacket.png"
+      />
+      <AlbumCard
+        name="Calm-Remix Nation"
+        url="http://youhardcore.web.fc2.com/release/018/index.html"
+        cover-url="https://www.getsumenhikou.com/content/images/jacket/Calm-Remix_Nation_jacket.jpg"
+      />
+      <AlbumCard
+        name="耽美主義者マルグリット"
+        url="http://www.getsumenhikou.com/marguerite/"
+        cover-url="https://www.getsumenhikou.com/content/images/jacket/marguerite_jacket.png"
+      />
+      <AlbumCard
+        name="ハジマリの音"
+        url="http://milkclip.web.fc2.com/htdocs/index.html"
+        cover-url="https://www.getsumenhikou.com/content/images/jacket/hajimari_jacket.jpg"
+      />
+      <AlbumCard
+        name="創作的な活動の時間 E.P."
+        url="http://www.getsumenhikou.com/sousaku-time/"
+        cover-url="https://www.getsumenhikou.com/content/images/jacket/sousaku-time.jpg"
+      />
+      <AlbumCard
+        name="東方長髪娘 〜LongHairGirls〜"
+        url="http://fp3.dojin.com/project04/"
+        cover-url="https://www.getsumenhikou.com/content/images/jacket/longhairjacket.jpg"
+      />
+    </section>
   </div>
 </template>
